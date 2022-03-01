5028066559f92fdc4d33f661801c0dba03c198e7
Charles Callender <charlescallender.cc@gmail.com>
2022-02-28 21:33:36 -0800

    Added home and header component

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,15 +1,17 @@
 import Login from './components/Login';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className='App'>
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login/>}>
+        <Route exact path="/" element={<Login/>}></Route>
+        <Route path="/home" element={<><Home/> <Header/></>}></Route>
         
-        </Route>
       </Routes>
     </Router>
     </div>
