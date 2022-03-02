ad09ade6137cdb94da42661fc2660497410876ba
Charles Callender <charlescallender.cc@gmail.com>
2022-03-01 23:44:27 -0800

    Added home sections

--- linkedin-clone/src/components/Home.js
+++ linkedin-clone/src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = (props) => {
     return (
@@ -7,6 +10,11 @@ const Home = (props) => {
                 <h5><a>Hiring in a Hurry?</a></h5>
                 <p>Find talented pros in record time with Upwork and keep businees moving.</p>
             </Section>
+            <Layout>
+               <Leftside/>
+               <Main/>
+               <Rightside/>
+            </Layout>
         </Container>
     )
 };
@@ -52,6 +60,22 @@ const Section = styled.section`
         padding: 0 5px;
     }
 
+`;
+
+
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside main rightside";
+    grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    /*grid-template-rows: auto;*/
+    margin: 25px 0;
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding: 0 5px;
+    }
 `;
 
 export default Home;
