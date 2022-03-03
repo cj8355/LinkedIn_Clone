eedf96d289009b6f4f80a0719874cd691afc4471
Charles Callender <charlescallender.cc@gmail.com>
2022-03-02 20:51:30 -0800

    Added rightside section

--- linkedin-clone/src/components/Main.js
+++ linkedin-clone/src/components/Main.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Main = (props) => {
     return (
         <Container>
-            Main
+            <ShareBox>Share</ShareBox>
         </Container>
     )
 };
@@ -12,6 +12,25 @@ const Main = (props) => {
 
 const Container = styled.div`
     grid-area: main;
-`
+`;
+
+const CommonCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: relative;
+    border: none;
+    box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+const ShareBox = styled(CommonCard)`
+    display: flex;
+    flex-direction: column;
+    color: #958b7b;
+    margin: 0 0 8px;
+    background: white;
+`;
 
 export default Main;
