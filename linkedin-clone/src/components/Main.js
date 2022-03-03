0287dbe3fe2a99d95e8864f13233e21870d90c94
Charles Callender <charlescallender.cc@gmail.com>
2022-03-02 21:12:09 -0800

    Added sharebox

--- linkedin-clone/src/components/Main.js
+++ linkedin-clone/src/components/Main.js
@@ -4,7 +4,34 @@ import styled from "styled-components";
 const Main = (props) => {
     return (
         <Container>
-            <ShareBox>Share</ShareBox>
+            <ShareBox>Share
+            <div>
+                <img src="/images/user.svg"></img>
+                <button>Start a post</button>
+            </div>
+
+            <div>
+                <button>
+                    <img src="/images/photo-icon.svg"></img>
+                    <span>Photo</span>
+                </button>
+
+                <button>
+                    <img src="/images/video-icon.svg"></img>
+                    <span>Video</span>
+                </button>
+
+                <button>
+                    <img src="/images/event-icon.svg"></img>
+                    <span>Event</span>
+                </button>
+
+                <button>
+                    <img src="/images/article-icon.svg"></img>
+                    <span>Write article</span>
+                </button>
+            </div>
+            </ShareBox>
         </Container>
     )
 };
@@ -31,6 +58,58 @@ const ShareBox = styled(CommonCard)`
     color: #958b7b;
     margin: 0 0 8px;
     background: white;
+
+    div {
+        button {
+            outline: none;
+            color: rgba(0, 0, 0, 0.6);
+            font-size: 14px;
+            line-height: 1.5;
+            min-height: 48px;
+            background: transparent;
+            border: none;
+            display: flex;
+            align-items: center;
+            font-weight: 600;
+        }
+        &:first-child {
+            display: flex;
+            align-items: center;
+            padding: 8px 16px 0 16px;
+
+            img {
+                width: 48px;
+                border-radius: 50%;
+                margin-right: 8px;
+            }
+
+            button {
+                margin: 4px 0;
+                flex-grow: 1;
+                border-radius: 35px;
+                padding-left: 16px;
+                border: 1px solid rgba(0, 0, 0, 0.15);
+                background-color: white;
+                text-align: left;
+            }
+        }
+        &:nth-child(2) {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            padding-bottom: 4px;
+
+            button {
+                img {
+                    margin: 0 4px 0 -2px; 
+                }
+
+                span {
+                    color: #6591d3;
+                }
+            }
+        }
+    }
 `;
 
 export default Main;
